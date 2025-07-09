fbe2951b9ac8d1aaab0d36dc1b69298044eaed8c
Imetomi <imetstamas@gmail.com>
2025-07-09 14:02:34 +0200

    fixed and removed app completely, only landing and CMS remains

--- packages/app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    externalDir: true,
-  },
-  transpilePackages: ['@bugninja/shared-ui'],
-}
-
-module.exports = nextConfig 
